9eaa8980759a3e96f3854af6578f197e91d9b9a5
Tanmay <tanmayawasthi105@gmail.com>
2019-09-21 23:02:58 +0530

    Added reference dependency injection

---
+++ springcore/src/main/java/com/tanmay/spring/reference/Scores.java
@@ -0,0 +1,39 @@
+package com.tanmay.spring.reference;
+
+/**
+ * @author Tanmay
+ * @date 21/09/19
+ **/
+public class Scores {
+    private Double maths;
+    private Double physics;
+    private Double chemistry;
+
+    public Double getMaths() {
+        return maths;
+    }
+
+    public void setMaths(Double maths) {
+        this.maths = maths;
+    }
+
+    public Double getPhysics() {
+        return physics;
+    }
+
+    public void setPhysics(Double physics) {
+        this.physics = physics;
+    }
+
+    public Double getChemistry() {
+        return chemistry;
+    }
+
+    public void setChemistry(Double chemistry) {
+        this.chemistry = chemistry;
+    }
+
+    public String toString() {
+        return "MATHS " + this.getMaths() + " PHYSICS " + this.getPhysics() + " CHEMISTRY " + this.getChemistry();
+    }
+}
---
+++ springcore/src/main/java/com/tanmay/spring/reference/Student.java
@@ -0,0 +1,17 @@
+package com.tanmay.spring.reference;
+
+/**
+ * @author Tanmay
+ * @date 21/09/19
+ **/
+public class Student {
+    public Scores getScores() {
+        return scores;
+    }
+
+    public void setScores(Scores scores) {
+        this.scores = scores;
+    }
+
+    private Scores scores;
+}
