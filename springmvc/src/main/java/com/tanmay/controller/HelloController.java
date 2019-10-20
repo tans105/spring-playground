b919d8b209a200fe8904ffd7fcc9d6612249ae24
Tanmay <tanmayawasthi105@gmail.com>
2019-10-20 17:26:43 +0530

    init spring mvc introduction

---
+++ springmvc/src/main/java/com/tanmay/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.tanmay.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+class HelloController {
+
+    @RequestMapping("/hello")
+    public ModelAndView hello() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("hello");
+        return modelAndView;
+    }
+}
